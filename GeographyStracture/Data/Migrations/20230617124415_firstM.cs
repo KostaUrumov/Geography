7ff58eb757ba38a2fa7b17cc01dad765bc223a85
KostaUrumov <koceurumov@gmail.com>
2023-06-17 15:54:15 +0000

    add more fields

---
+++ GeographyStracture/Data/Migrations/20230617124415_firstM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeorgaphyStructure.Data.Migrations
+{
+    public partial class firstM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
