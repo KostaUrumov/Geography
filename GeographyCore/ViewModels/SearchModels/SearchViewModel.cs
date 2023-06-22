049d0f884104eeca58fdcf4f82d458812080415f
Kostadin Urumov <koceurumov@gmail.com>
2023-06-22 21:46:23 +0300

    search functon Get done

---
+++ GeographyCore/ViewModels/SearchModels/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GeographyCore.ViewModels.SearchModels
+{
+    public class SearchViewModel
+    {
+        public List<string>? listed { get; set; } = new List<string>();
+
+        public string Find { get; set; } = null!;
+
+        public int SelectionOfTheList { get; set; }
+    }
+}
---
+++ Georgaphy/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using GeographyCore.ViewModels.SearchModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Packaging;
+
+namespace Georgaphy.Controllers
+{
+    [Authorize]
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Search()
+        {
+            
+
+            var model = new SearchViewModel()
+            {
+               listed = new List<string>
+               { "Continent",
+                 "Country", 
+                 "River", 
+                 "City", 
+                 "Mountain"
+               }
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Search(SearchViewModel model)
+        {
+
+            return View();
+        }
+    }
+}
